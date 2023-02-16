26bcda731f08dd72e7af1d4bc39ef8fdc4ebea72
MrBoobai <e-gaugro@iskule.no>
2023-02-16 12:00:13 +0100

    started on colors

--- slot_maschine.py
+++ slot_maschine.py
@@ -1,48 +1,63 @@
-from art import *
-import random
 import os
+import random
 import subprocess
 import time
 
-#* function to check if input is a number
+from art import *
+
+"""
+todo:
+flashing colors
+arcade sound
+highscore for length
+highscore for amount won
+?jackpot
+!wallet can be zero but not negative
+"""
+
+# * function to check if input is a number
 def is_number(value):
     if isinstance(value, str):
         return value.isdigit()
     return isinstance(value, (int, float))
 
-#* delay for visual effects
+
+# * delay for visual effects
 delay = 2
 
-#* boot
+# * boot
+print("\033[40m")
+print("\033[33m")
 tprint("Emoji_machine")
+print("\033[0m")
 
-#* insert wallet
+# * insert wallet
 wallet = random_number = random.randint(1, 100)
-#* input("Insert Valid Amount of coins ... ")
-print("current wallet amount {}".format(wallet))
+# * input("Insert Valid Amount of coins ... ")
+print("\033[43mcurrent wallet amount {}\033[0m".format(wallet))
 
 while True:
-    #* bet amount
-    spent = input("How Much To Bet one, two Or four. ")
+    # * bet amount
+    spent = input("How Much To Bet \033[41m one \033[0m, \033[43m two \033[0m Or \033[42m four \033[0m. ")
     if spent in ["1", "2", "4"]:
         bool(True)
     else:
         exit()
 
-    #* Convert the spent value to an integer
+    # * Convert the spent value to an integer
     spent = int(spent)
 
     if wallet > spent:
         wallet = wallet - spent
     else:
-        print("Insufficient Funds. (Wallet Cannot Be Zero)")
+        print(" \033[31m Insufficient Funds. (Wallet Cannot Be Zero) \033[0m")
         exit()
 
-    #* spin
+    # * spin
     spin1 = random_number = random.randint(1, 10)
     spin2 = random_number = random.randint(1, 10)
     spin3 = random_number = random.randint(1, 10)
-    #* payout calculation
+    # * payout calculation
     if spin1 == 10:
         payout1 = spent * 3
     else:
@@ -55,8 +70,8 @@ while True:
         payout3 = spent * 3
     else:
         payout3 = 0
-    
-    #* show result (slots icons)
+
+    # * show result (slots icons)
     time.sleep(1)
     if spin1 == 10:
         print("{}".format(spin1))
@@ -73,9 +88,9 @@ while True:
     else:
         print(spin3)
 
-    #* payout
+    # * payout
     payout = payout1 + payout2 + payout3
-    
+
     if payout > 0:
         print("Your payout is ... ", end=" ")
         time.sleep(3)
@@ -84,25 +99,9 @@ while True:
         print("Your payout is ... ", end=" ")
         time.sleep(3)
         print(payout)
-    
+
     wallet = payout + wallet
 
-    #* show wallet
+    # * show wallet
     print("current wallet amount {}".format(wallet))
     continue
-
-"""
-todo:
-flashing colors
-arcade sound
-highscore for length
-highscore for amount won
-?jackpot
-wallet can be zero but not negative
-!
-?
-//
-todo
-*
-FIXME:
-"""
