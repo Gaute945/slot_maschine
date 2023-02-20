3ac111cb11f927bf0981ceffe8d1ed5eb99f3acf
MrBoobai <e-gaugro@iskule.no>
2023-02-20 10:23:34 +0100

    sync

--- slot_maschine.py
+++ slot_maschine.py
@@ -7,13 +7,12 @@ from art import *
 
 """
 todo:
-flashing colors
+//colors
 arcade sound
-highscore for length
+*highscore for length
 highscore for amount won
 database for amount house has won
 ?jackpot
-!wallet can be zero but not negative
 """
 
 # * function to check if input is a number
@@ -35,12 +34,14 @@ print("\033[0m")
 # * insert wallet
 wallet = random_number = random.randint(1, 100)
 # * input("Insert Valid Amount of coins ... ")
-print("\033[43mcurrent wallet amount {}\033[0m".format(wallet))
+print("\033[43m wallet amount {}\033[0m".format(wallet))
+rounds = 0
+score = 0
 
 while True:
     # * bet amount
     spent = input(
-        "How Much To Bet \033[41m one \033[0m, \033[43m two \033[0m Or \033[42m four \033[0m. "
+        "Bet \033[41m one \033[0m, \033[43m two \033[0m Or \033[42m four \033[0m. "
     )
     if spent in ["1", "2", "4"]:
         bool(True)
@@ -50,27 +51,34 @@ while True:
     # * Convert the spent value to an integer
     spent = int(spent)
 
+    #* adjust balance
     if wallet > spent:
         wallet = wallet - spent
     else:
-        print(" \033[31m Insufficient Funds. (Wallet Cannot Be Zero) \033[0m")
+        print(" \033[31m Insufficient funds (Wallet Cannot Be Zero) \033[0m")
+        print("Try to beat round {}!".format(rounds))
         exit()
 
     # * spin
     spin1 = random_number = random.randint(1, 10)
     spin2 = random_number = random.randint(1, 10)
     spin3 = random_number = random.randint(1, 10)
+    # * round/score count
+    rounds += 1
     # * payout calculation
     if spin1 == 10:
         payout1 = spent * 3
+        payout1 + score
     else:
         payout1 = 0
     if spin2 == 10:
-        payout2 = spent * 3
+        payout2 = spent * 4
+        payout2 + score
     else:
         payout2 = 0
     if spin3 == 10:
-        payout3 = spent * 4
+        payout3 = spent * 5
+        payout3 + score
     else:
         payout3 = 0
 
@@ -95,7 +103,7 @@ while True:
     payout = payout1 + payout2 + payout3
 
     if payout > 0:
-        print("Your payout is ... ", end=" ")
+        print("Payout is ... ", end=" ")
         time.sleep(3)
         print("\u001b[32m{}\u001b[0m".format(payout))
     else:
@@ -104,5 +112,10 @@ while True:
     wallet = payout + wallet
 
     # * show wallet
-    print("current wallet amount \u001b[32m{}\u001b[0m".format(wallet))
-    
+    time.sleep(delay)
+    print("Current wallet amount \u001b[32m{}\u001b[0m".format(wallet))
+    #* round counter
+    print("Round \u001b[32m{}\u001b[0m".format(rounds))
+    #* highscore score
+    print("Highscore \u001b[32m{}\u001b[0m".format(score))
+    time.sleep(delay)
